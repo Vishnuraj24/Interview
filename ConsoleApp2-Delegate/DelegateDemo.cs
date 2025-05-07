614c2b49a75b90354145fdc322f51d51ad5aff44
Vishnuraj24 <117718708+Vishnuraj24@users.noreply.github.com>
2025-05-07 07:57:05 +0530

    delegate

---
+++ ConsoleApp2-Delegate/DelegateDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_Delegate
+{
+    delegate void PrintDelegate(string message);
+    public class DelegateDemo
+    {
+        public static void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public static void TestMethod() {
+            PrintDelegate printDelegate = PrintMessage;
+            printDelegate("Hi vishnuraj Kandukuri");
+        }
+    }
+}
