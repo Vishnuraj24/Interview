2e3e0a510ca2ea9b5e6f969ea4a5b825abd7206d
Vishnuraj24 <117718708+Vishnuraj24@users.noreply.github.com>
2025-05-07 07:48:38 +0530

    ADO

---
+++ ADO/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ADO.DataAccess_ADO;
+
+Console.WriteLine("Hello, World!");
+
+
+DataAccess dataAccess = new DataAccess();
+dataAccess.GetData();
+dataAccess.GetDataById(4);
+dataAccess.GetCount();
